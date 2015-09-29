98149c53959063ecb84e750090594576a19bcd41
madflytom@gmail.com <madflytom@gmail.com>
2015-09-28 22:11:38 -0400

    Initial Commit

---
+++ InvestigatorReq/Models/Requisition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvestigatorReq.Models
+{
+    public class Requisition
+    {
+        String AccessionID { get; set; }
+        List<object> ReqQuestions { get; set; }
+    }
+}
---
+++ InvestigatorReq/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InvestigatorReq.Startup))]
+namespace InvestigatorReq
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
